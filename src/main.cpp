d88763be8730a7dcb30521bf94cd533c7835a26d
MYMutawa <MYMutawa@gmail.com>
2024-09-11 18:08:42 +0300

    User Controls Working

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,80 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       moham                                                     */
-/*    Created:      9/7/2024, 1:08:21 PM                                      */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
 
 #include "vex.h"
 
 using namespace vex;
+brain Brain;
+motor MotorLF = motor(PORT9);
+motor MotorLB = motor(PORT1);
+motor MotorRF = motor(PORT10);
+motor MotorRB = motor(PORT2);
+controller Controller = controller();
 
-// A global instance of competition
 competition Competition;
 
-// define your global instances of motors and other devices here
-
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
-
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+  Brain.Screen.print("Pre Auto VEX");
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+
 
 void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
+  Brain.Screen.print("Hello KFUPM VEX");
+  Brain.Screen.print("Autonomous VEX");
+  //Go stright
+  MotorLF.spin(forward);
+  MotorLB.spin(forward);
+  MotorRF.spin(forward);
+  MotorRB.spin(forward);
+
+vexDelay(3000);
+
+  //Go Back
+  MotorLF.spin(reverse);
+  MotorLB.spin(reverse);
+  MotorRF.spin(reverse);
+  MotorRB.spin(reverse);
+
+  vexDelay(3000);
+
+    //Go Right
+   MotorLF.spin(forward);
+  MotorLB.spin(forward);
+  MotorRF.spin(reverse);
+  MotorRB.spin(reverse);
+  vexDelay(3000);
+
+     //Go left
+  MotorLF.spin(reverse);
+  MotorLB.spin(reverse);
+  MotorRF.spin(forward);
+  MotorRB.spin(forward);
+  vexDelay(3000);
+  MotorLF.stop();
+  MotorLB.stop();
+  MotorRF.stop();
+  MotorRB.stop();
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+void axisChanged() {
+  int32_t drive_left;
+  int32_t drive_right;
+    drive_left  =  Controller.Axis3.position();
+    drive_right =  Controller.Axis2.position();
+    MotorLF.spin(forward, drive_left, percent);
+    MotorLB.spin(forward, drive_left, percent);
+    MotorRB.spin(forward, drive_right, percent);
+    MotorRF.spin(forward, drive_right, percent);
+    }
+
 
 void usercontrol(void) {
-  // User control code here, inside the loop
   while (1) {
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
-
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
-
-    wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
+  Controller.Axis1.changed(axisChanged);
+  Controller.Axis2.changed(axisChanged);
+  Controller.Axis3.changed(axisChanged);
+  Controller.Axis4.changed(axisChanged);
+  wait(20, msec); 
   }
 }
 
@@ -82,10 +85,10 @@ int main() {
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
-
   // Run the pre-autonomous function.
   pre_auton();
-
+  autonomous();
+  usercontrol();
   // Prevent main from exiting with an infinite loop.
   while (true) {
     wait(100, msec);

342b825f6dc3943da3afec7b043d3f77628bfbbc
MYMutawa <MYMutawa@gmail.com>
2024-09-16 13:27:27 +0300

    small-robot-file

--- src/smallmain.cpp
+++ src/smallmain.cpp
@@ -1,12 +1,9 @@
 
-#include "math.h"
 #include "vex.h"
 
 using namespace vex;
 brain Brain;
-motor MotorLF = motor(PORT9);
 motor MotorLB = motor(PORT1);
-motor MotorRF = motor(PORT10);
 motor MotorRB = motor(PORT2);
 controller Controller = controller();
 
@@ -23,34 +20,25 @@ void autonomous(void) {
   Brain.Screen.print("Hello KFUPM VEX");
   Brain.Screen.print("Autonomous VEX");
   //Go stright
-  MotorLF.spin(forward);
   MotorLB.spin(forward);
-  MotorRF.spin(forward);
   MotorRB.spin(forward);
-
-vexDelay(3000);
+  vexDelay(3000);
 
   //Go Back
-  MotorLF.spin(reverse);
   MotorLB.spin(reverse);
-  MotorRF.spin(reverse);
   MotorRB.spin(reverse);
-
   vexDelay(3000);
 
     //Go Right
-   MotorLF.spin(forward);
   MotorLB.spin(forward);
-  MotorRF.spin(reverse);
   MotorRB.spin(reverse);
   vexDelay(3000);
 
      //Go left
-  MotorLF.spin(reverse);
   MotorLB.spin(reverse);
-  MotorRF.spin(forward);
   MotorRB.spin(forward);
   vexDelay(3000);
+
   MotorLF.stop();
   MotorLB.stop();
   MotorRF.stop();
@@ -62,10 +50,8 @@ void axisChanged() {
   int32_t drive_right;
     drive_left  =  Controller.Axis3.position();
     drive_right =  Controller.Axis2.position();
-    MotorLF.spin(forward, drive_left, percent);
     MotorLB.spin(forward, drive_left, percent);
     MotorRB.spin(forward, drive_right, percent);
-    MotorRF.spin(forward, drive_right, percent);
     }
 
 
